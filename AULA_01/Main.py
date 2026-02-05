dc055cce57e2bb704dd510be8115bb062acc0175
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2026-02-05 14:48:15 -0300

    Remove redundant comment in dataset creation

--- AULA_01/Main.py
+++ AULA_01/Main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 1. Criação do Dataset Fictício (Simulando o CSV)
+# 1. Criação do Dataset Fictício
 data = {
     'id_transacao': [101, 102, 103, 104, 105, 106, 107, 108, 109, 110],
     'valor': [150, 450, 200, 800, 350, 120, 950, 600, 280, 720],
